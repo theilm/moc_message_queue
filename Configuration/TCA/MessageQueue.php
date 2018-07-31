7252bf6edcf0e43e2de21e0e6f5ec237c78559f3
Michael Telgkamp <michael.telgkamp@mindscreen.de>
2018-07-31 10:58:22 +0200

    Adjust ext_tables.php to not contain TCA adjustments
    Breaking: https://forge.typo3.org/issues/78384 - Frontend ignores TCA in ext_tables.php

--- Configuration/TCA/MessageQueue.php
+++ Configuration/TCA/MessageQueue.php
@@ -1,16 +1,27 @@
 <?php
-$TCA['tx_mocmessagequeue_queue'] = array(
-	'ctrl' => $TCA['tx_mocmessagequeue_queue']['ctrl'],
-	'interface' => array('showRecordFieldList' => 'data'),
-	'columns' => array(
-		'data' => array(
-			'label' => 'Serialized event',
-			'config' => array(
-				'type' => 'passthrough',
-			),
-		)
-	),
-	'types' => array(
-		'0' => array('showitem' => 'data')
-	)
-);
+$TCA['tx_mocmessagequeue_queue'] = [
+    'ctrl' => [
+        'title' => 'MOC Message queue',
+        'label' => 'Message',
+        'tstamp' => 'tstamp',
+        'crdate' => 'crdate',
+        'cruser_id' => 'cruser_id',
+        'hideTable' => 1,
+        'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) .
+            'Configuration/TCA/MessageQueue.php',
+        'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) .
+            'Resources/Public/Icons/MessageQueue.png'
+    ],
+    'interface' => ['showRecordFieldList' => 'data'],
+    'columns' => [
+        'data' => [
+            'label' => 'Serialized event',
+            'config' => [
+                'type' => 'passthrough',
+            ],
+        ]
+    ],
+    'types' => [
+        '0' => ['showitem' => 'data']
+    ]
+];
