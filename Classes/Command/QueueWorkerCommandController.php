d60a18055dfe398aaf02090a6ab0df5a39f31718
Michael Telgkamp <michael.telgkamp@mindscreen.de>
2018-09-27 12:25:08 +0200

    Extend logging, write logs using typo3 logger

--- Classes/Command/QueueWorkerCommandController.php
+++ Classes/Command/QueueWorkerCommandController.php
@@ -1,10 +1,15 @@
 <?php
+
 namespace MOC\MocMessageQueue\Command;
 
 use MOC\MocMessageQueue\Message\MessageInterface;
 use MOC\MocMessageQueue\Message\StringMessage;
+use MOC\MocMessageQueue\Queue\QueueInterface;
+use TYPO3\CMS\Core\Log\Logger;
+use TYPO3\CMS\Core\Log\LogManager;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\CommandController;
+use TYPO3\CMS\Extbase\SignalSlot\Dispatcher;
 
 /**
  * Message queue worker
@@ -13,75 +18,131 @@ use TYPO3\CMS\Extbase\Mvc\Controller\CommandController;
  *
  * @package MOC\MocMessageQueue
  */
-class QueueWorkerCommandController extends CommandController {
+class QueueWorkerCommandController extends CommandController
+{
 
-	/**
-	 * @var \MOC\MocMessageQueue\Queue\QueueInterface
-	 * @inject
-	 */
-	protected $queue;
+    /**
+     * @var QueueInterface
+     */
+    protected $queue;
 
-	/**
-	 * @var \TYPO3\CMS\Extbase\SignalSlot\Dispatcher
-	 * @inject
-	 */
-	protected $signalSlotDispatcher;
+    /**
+     * @var Dispatcher
+     */
+    protected $signalSlotDispatcher;
 
-	/**
-	 * Run the queue in the background
-	 *
-	 * @param integer $maximumMessages If set to a number larger than o (default), only this amount of numbers are handed,
-	 * before it exits with an exit code of 9
-	 * @param boolean $debugOutput If TRUE, a slot is connected that display some debug output when a message is handled
-	 * @return void
-	 */
-	public function startCommand($maximumMessages = 0, $debugOutput = FALSE) {
-		if ($debugOutput) {
-			print date('d/m-Y H:i:s') .  ' Starting up queue worker with implementation ' . get_class($this->queue) . PHP_EOL;
-			$this->signalSlotDispatcher->connect(__CLASS__, 'messageReceived', function(MessageInterface $message) {
-				print date('d/m-Y H:i:s') .  ' Message received: ' . get_class($message);
-				if ($message instanceof StringMessage) {
-					print ' - Message ' . $message->getPayload();
-				}
-				print PHP_EOL;
-			});
-		}
+    /**
+     * @var Logger
+     */
+    private $logger;
 
-		$numberOfMessagesHandled = 0;
-		while (TRUE) {
-			try {
-				$message = $this->queue->waitAndReserve();
-				if ($message !== NULL) {
-					$this->signalSlotDispatcher->dispatch(__CLASS__, 'messageReceived', array(
-						'message' => $message
-					));
-					$this->queue->finish($message);
-					$numberOfMessagesHandled++;
-					if ($maximumMessages > 0 && $numberOfMessagesHandled >= $maximumMessages) {
-						print date('d/m-Y H:i:s') .  ' Maximum number of messages ' . $maximumMessages . ' is reached. Exiting with exitcode 9.' . PHP_EOL;
-						$this->sendAndExit(9);
-					}
-				}
-			} catch (\Exception $exception) {
-				if ($debugOutput) {
-					print ' - Error handling message:' . $exception->getMessage() . PHP_EOL;
-				}
-				GeneralUtility::devLog('Error handling message:' . $exception->getMessage(), 'moc_message_queue');
-			}
-		}
-	}
+    /**
+     * QueueWorkerCommandController constructor.
+     */
+    public function __construct()
+    {
+        $this->logger = GeneralUtility::makeInstance(LogManager::class)->getLogger(__CLASS__);
+    }
 
-	/**
-	 * Publish test message to queue
-	 *
-	 * This will publish a simple StringMessage to the queue. It is only used for test purposes.
-	 *
-	 * @param string $messageString The message to publish
-	 * @return void
-	 */
-	public function publishTestMessageCommand($messageString) {
-		$message = new StringMessage($messageString);
-		$this->queue->publish($message);
-	}
+    /**
+     * @param QueueInterface $queue
+     */
+    public function injectQueue(QueueInterface $queue)
+    {
+        $this->queue = $queue;
+    }
+
+    /**
+     * @param Dispatcher $signalSlotDispatcher
+     */
+    public function injectSignalSlotDispatcher(Dispatcher $signalSlotDispatcher)
+    {
+        $this->signalSlotDispatcher = $signalSlotDispatcher;
+    }
+
+    /**
+     * Run the queue in the background
+     *
+     * @param integer $maximumMessages If set to a number larger than o (default), only this amount of numbers are handed,
+     * before it exits with an exit code of 9
+     * @param boolean $debugOutput If TRUE, a slot is connected that display some debug output when a message is handled
+     * @return void
+     */
+    public function startCommand($maximumMessages = 0, $debugOutput = false)
+    {
+        $this->logger->debug(
+            'Starting up queue worker with implementation ' . get_class($this->queue)
+        );
+        $this->signalSlotDispatcher->connect(
+            __CLASS__,
+            'messageReceived',
+            function (MessageInterface $message) {
+                $this->logger->debug(
+                    'Message received: ' . get_class($message) .
+                    ($message instanceof StringMessage ? ' - Message ' . $message->getPayload() : '')
+                );
+            }
+        );
+        if ($debugOutput) {
+            print date('d/m-Y H:i:s ') .  'Starting up queue worker with implementation ' . get_class($this->queue) . PHP_EOL;
+            $this->signalSlotDispatcher->connect(__CLASS__, 'messageReceived', function(MessageInterface $message) {
+                print date('d/m-Y H:i:s ') .  'Message received: ' . get_class($message);
+                if ($message instanceof StringMessage) {
+                    print ' - Message ' . $message->getPayload();
+                }
+                print PHP_EOL;
+            });
+        }
+
+        $numberOfMessagesHandled = 0;
+        while (true) {
+            try {
+                $message = $this->queue->waitAndReserve();
+                if ($message !== null) {
+                    $this->signalSlotDispatcher->dispatch(
+                        __CLASS__,
+                        'messageReceived',
+                        ['message' => $message]
+                    );
+                    $this->queue->finish($message);
+                    $numberOfMessagesHandled++;
+                    if ($maximumMessages > 0 && $numberOfMessagesHandled >= $maximumMessages) {
+                        $this->logger->debug(
+                            'Maximum number of messages ' . $maximumMessages . ' is reached. Exiting with exitcode 9.'
+                        );
+                        print date('d/m-Y H:i:s ') .  'Maximum number of messages ' . $maximumMessages . ' is reached. Exiting with exitcode 9.' . PHP_EOL;
+                        $this->sendAndExit(9);
+                    }
+                } else {
+                    $this->logger->error('Message is null. Exiting with exitcode 9.');
+                    $this->sendAndExit(9);
+                }
+            } catch (\Exception $exception) {
+                $logMessage = 'Error handling message:' . $exception->getMessage();
+                $this->logger->error($logMessage);
+                if ($debugOutput) {
+                    print date('d/m-Y H:i:s ') .  $logMessage . PHP_EOL;
+                }
+                GeneralUtility::devLog(
+                    $logMessage,
+                    'moc_message_queue'
+                );
+            }
+        }
+    }
+
+    /**
+     * Publish test message to queue
+     *
+     * This will publish a simple StringMessage to the queue. It is only used for test purposes.
+     *
+     * @param string $messageString The message to publish
+     * @return void
+     */
+    public function publishTestMessageCommand($messageString)
+    {
+        $message = new StringMessage($messageString);
+        $this->queue->publish($message);
+    }
 
 }
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,6 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
+    die('Access denied.');
 }
 
 $config = unserialize($_EXTCONF);
@@ -8,20 +8,39 @@ $config = unserialize($_EXTCONF);
 \MOC\MocMessageQueue\Queue\BeanstalkQueue::$tube = $config['beanstalk_tube'];
 
 if ($config['message_queue_implementation'] === 'Beanstalk' && $config['disable_pheanstalk_import'] !== "1") {
-	$pheanstalkClassRoot = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . '/Classes';
-	require_once($pheanstalkClassRoot . '/Pheanstalk/ClassLoader.php');
-	Pheanstalk_ClassLoader::register($pheanstalkClassRoot);
+    $pheanstalkClassRoot = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . '/Classes';
+    require_once($pheanstalkClassRoot . '/Pheanstalk/ClassLoader.php');
+    Pheanstalk_ClassLoader::register($pheanstalkClassRoot);
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'MOC\MocMessageQueue\Command\QueueWorkerCommandController';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] =
+    'MOC\MocMessageQueue\Command\QueueWorkerCommandController';
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('
 config.tx_extbase {
-	objects {
-		MOC\MocMessageQueue\Queue\QueueInterface {
-			className = MOC\\MocMessageQueue\\Queue\\' . $config['message_queue_implementation']. 'Queue
-		}
-	}
+    objects {
+        MOC\MocMessageQueue\Queue\QueueInterface {
+            className = MOC\\MocMessageQueue\\Queue\\' . $config['message_queue_implementation'] . 'Queue
+        }
+    }
 }
 ');
 
+// Logging configuration for mocMessageQueue
+$GLOBALS['TYPO3_CONF_VARS']['LOG']['MOC']['MocMessageQueue']['Command']
+['QueueWorkerCommandController']['writerConfiguration'] = [
+    // configuration for DEBUG severity, including all
+    // levels with higher severity
+    \TYPO3\CMS\Core\Log\LogLevel::DEBUG => [
+        // add a SyslogWriter
+        \TYPO3\CMS\Core\Log\Writer\FileWriter::class => [
+            'logFile' => 'typo3temp/var/logs/moc-message-queue-debug.log'
+        ],
+    ],
+    \TYPO3\CMS\Core\Log\LogLevel::ERROR => [
+        // add a SyslogWriter
+        \TYPO3\CMS\Core\Log\Writer\FileWriter::class => [
+            'logFile' => 'typo3temp/var/logs/moc-message-queue-error.log'
+        ],
+    ],
+];
